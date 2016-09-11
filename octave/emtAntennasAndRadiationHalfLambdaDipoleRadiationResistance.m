25601a18a44647360dd030b81167813e6ee4d7d6
khalid yousafzai <khalidyousafzai@comsats.edu.pk>
2016-09-11 20:01:05 +0500

    travelling wave antenna

--- octave/emtAntennasAndRadiationHalfLambdaDipoleRadiationResistance.m
+++ octave/emtAntennasAndRadiationHalfLambdaDipoleRadiationResistance.m
@@ -11,7 +11,19 @@ d=lmda./2;
 B=2.*pi./lmda;
 
 function y=f(x)
-y=60*(cos(pi./2.*cos(x))).^2./sin(x);
+y=30*(cos(pi./2.*cos(x))).^2./sin(x);
 endfunction
 
 [v, ier, nfun, err] = quad ("f", 0, pi)
+
+x=0.1:0.1:0.9
+f(pi*x)
+
+sum=0;
+for entry=x;
+sum=sum+f(entry*pi);
+end;
+
+sum.*0.1.*pi
+
+#plot (x,f(x*pi));
