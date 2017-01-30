4b414f9069d83120ff8d442885357b5906b250af
khalid yousafzai <khalidyousafzai@comsats.edu.pk>
2017-01-30 12:33:55 +0500

    Remove temporary and fig/testing folders

--- fig/testing/kkkOctaveFunctionData.m
+++
@@ -1,8 +0,0 @@
-kread=fopen("k.dat","w");
-
-for x=[2,3,4,5,6,7,8,9];
-y=x^2-3*x+2;
-fprintf(kread,"%i %s %i",x,"    ",y); fprintf(kread,"\n")
-endfor
-
-fclose(kread)
--- fig/testing/octaveFunctionCall.parabola.gnuplot
+++
@@ -1,2 +0,0 @@
-set table "octaveFunctionCall.parabola.table"; set format "%.5f"
-set samples 25; plot [x=3:9.2] 0.1*x**2 + 2.5
